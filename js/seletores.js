771ceb6cce69fb8b5db37b99623e3f1b35f5a4c8
AlefeVariani <alefevariani@unochapeco.edu.br>
2014-07-15 23:21:01 -0300

    Primeiro commit

---
+++ js/seletores.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+
+});
+
+$(function() { 
+
+});
+
+console.log($('p.comment-meta *')); //Pega todos os elementos dentro de p.comment-meta
+
+console.log($('div')); // Pega todas tags div
+
+
+
