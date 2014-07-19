8f5b433da413c513f7b1d7980bb8bff120c7dd9c
AlefeVariani <alefevariani@unochapeco.edu.br>
2014-07-19 19:17:42 -0300

    novos selectores

--- js/seletores.js
+++ js/seletores.js
@@ -6,6 +6,9 @@ $(document).ready(function() {
 */
 $(function() { 
 
+// .text() pega o texto do elemento - Ex: <em>Span</em> :pega o "Span" 	
+// .val() pega o value(valor) do elemento - Ex: <option value="1">Opcao 1</option> :pega o "1" 	
+
 console.log($('p.comment-meta *')); // Pega todos os elementos dentro de p.comment-meta
 
 console.log($('div')); // Pega todas tags div
@@ -26,6 +29,23 @@ console.log($('option:selected').val()); // Pega opção que estiver selecionado
 
 console.log($('p strong').text()); // Pega elemento p que tenha filho strong
 
+console.log($('p > em').text()); // Pega elemento em dentro de p
+
+console.log($('h2 + p').text()); // Pega p imediatamente precedido de h2 
+
+console.log($('h1 ~ ul').text()); // Pega ul que seja precedido de h1 
+
+console.log($('h1,p,a')); //Pega todos os elementos informados, separados apenas por virgula
+
+console.log($(':checkbox[checked]')); //Pega elemento checkbox com atributo checked
+
+console.log($('[id^=div-vazia]')); //Pega atributo id que inicie com o value div-vazia
+
+console.log($('[id$=div]')); //Pega atributo id que termine com o value div-vazia
+
+console.log($('dd[class*=o]').text()); // Pega atributo dd que possua a class com o value "o" 
+
+console.log($('dd[class=cores]').text()); // Pega atributo dd que possua a class cores
 
 });
 
